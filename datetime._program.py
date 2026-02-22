dcf84cfeb50d9c501c8d9406216da80a2eb475cc
rastogidevansh-1838 <rastogi.kopal@gmail.com>
2026-02-22 10:13:54 +0530

    first commit

---
+++ datetime._program.py
@@ -0,0 +1,6 @@
+from datetime import date, time, datetime
+today = date.today()
+now = datetime.now()
+print("Today's date is", today)
+print("\nCurrent Date and Time is", now)
+print("\nDate Components", today.year, today.month, today.day)
